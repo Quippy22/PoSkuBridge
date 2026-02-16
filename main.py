522fbb1d447011845323e6e0ed718c096a3bf1a6
Quippy22 <darius.alexandru2202@gmail.com>
2026-02-16 21:03:15 +0200

    core: final cleanup

--- main.py
+++ main.py
@@ -1,42 +1,13 @@
-import threading
-
 from src.core.app import App
-from src.core.database import database as db
 from src.core.logger import setup_logging
-from src.core.settings import settings
 from src.gui.application import GUI
 from src.lib.files import setup_filesystem
-
-# Development tools
-from src.tools.po_generator import PoGenerator
-from src.tools.tests import (
-    database_test,
-    matcher_test,
-    registry_stress_test,
-    stress_test,
-)
-from src.tools.wipe_data import nuke_environment
 
 if __name__ == "__main__":
     # -- Infrastructure Setup --
     setup_filesystem()
     setup_logging()
-    # For testing, wipe all the files before starting
-    # nuke_environment()
 
     backend = App()
     gui = GUI(backend)
-
-    # The stress test
-    # threading.Thread(target=stress_test, daemon=True).start()
-
-    # The database test
-    # threading.Thread(target=database_test, daemon=True).start()
-
-    # Populate db for the matcher test
-    # threading.Thread(target=matcher_test, daemon=True).start()
-
-    # Populate db for the registry test
-    # threading.Thread(target=registry_stress_test, daemon=True).start()
-
     gui.mainloop()
